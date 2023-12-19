f88981c8ee057878976a9a4eac4a3cc8d8582f33
Robertas77z <Robertas77z@gmail.com>
2023-12-19 23:07:37 +0200

    test

--- src/lt/techin/Main.java
+++ src/lt/techin/Main.java
@@ -1,7 +1,28 @@
-package lt.techin;
+//package lt.techin;
+//import java.util.Scanner;
+//public class Main {
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("?");
-    }
-}
+//    public static void main(String[] args) {
+//        System.out.println("Welcome to Java");
+//        System.out.println("Learning Java Now");
+//        System.out.println("Programming is fun");
+//        System.out.println("  J     A   V       V   A");
+//        System.out.println("  J    A A   V     V   A A");
+//        System.out.println("  J   A A A   V   V   A A A");
+//        System.out.println("J J  A     A    V    A     A");
+//        System.out.println("a     a^2      a^3     a^4\n" +
+//                           "−−−−−−−−−−−−−−−−−−−−−−−−−−−\n" +
+//                           "1    1        1       1\n" +
+//                           "2    4        8       16\n" +
+//                           "3    9        27      81\n" +
+//                           "4    16       64      256");
+       /* Programa kui pasisveikina
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Įveskite savo vardą: ");
+        String vardas = scanner.nextLine();
+        System.out.println("Sveiki, " + vardas + "!");
+        scanner.close();*/
+
+//    }
+//}
+
